b5318024a33e1504726eac6f7dbe030d9b542a7a
orestisef <orestisef@gmail.com>
2020-04-03 19:17:10 +0300

    set up menu bar and google auth

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamList from './streams/StreamList';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamShow from './streams/StramShow';
+import Header from './header/Header';
+
 
 
 const App = () => {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className="ui container">
             <BrowserRouter>
                 <div>
+                <Header />
                 <Route path="/" exact component={ StreamList }  />
                 <Route path="/streams/new" exact component={ StreamCreate }  />
                 <Route path="/stream/edit" exact component={ StreamEdit }  />
